22f0719d8b187669cdb2abbf0f8b625976247709
amitkumar singh <amitkumarsingh@amitkumars-MacBook-Pro.local>
2023-07-03 00:28:17 -0400

    merge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,9 @@ const Footer = () => {
       <div className='socialMedia'>
         {/* <InstagramIcon />
         <TwitterIcon /> */}
-        <Link to="https://www.linkedin.com/in/amitbhsingh/"><LinkedInIcon /></Link>
-        <Link to="https://github.com/amitbhsingh"><GithubIcon /></Link>
-        <Link to="mailto:amit.bh.singh@gmail.com"><EmailIcon /></Link>
+        <Link to="https://www.linkedin.com/in/amitbhsingh/"target='_blank' rel='noreferrer'><LinkedInIcon /></Link>
+        <Link to="https://github.com/amitbhsingh"target='_blank' rel='noreferrer'><GithubIcon /></Link>
+        <Link to="mailto:amit.bh.singh@gmail.com"target='_blank' rel='noreferrer'><EmailIcon /></Link>
       </div>
       <p>&copy; 2023</p>
       <Link className='insta'></Link>
