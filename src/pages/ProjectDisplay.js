6a27668cf82e017edbfe852769430613f58070ee
Brendan Dagys <brendandagys@gmail.com>
2023-07-13 11:39:15 -0400

    Add ESLint dev dependency

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,22 +1,19 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/ProjectDisplay.css";
 
-function ProjectDisplay() {
+export const ProjectDisplay = () => {
   const { id } = useParams();
-  const project = ProjectList[id];
+  const project = ProjectList[  id];
   return (
     <a href={project.url}><div className="project">
       <h1> {project.name}</h1>
-      <img src={project.image} />
+      <img src={project.image} alt="my project" />
       <p>
         <b>Skills:</b> {project.skills}
       </p>
       <GitHubIcon />
     </div></a>
   );
-}
-
-export default ProjectDisplay;``
+};
